a0aa8e11078d7bacbc9ca481cabca5383db73a4f
Mitchell Youngerman <MYounge@am.trimblecorp.net>
2020-04-20 15:36:16 -0400

    Rewrote code so it runs faster and is shorter

--- Code.js
+++ Code.js
@@ -10,7 +10,7 @@ function onOpen()
 function opentitlePageDialog()
 {
   var html = HtmlService.createHtmlOutputFromFile('TitlePage'); // Create an HTML output using 'TitlePage.html'.
-  html.setTitle("Add Title Page Information"); // Set the title of the sidebar.
+  html.setTitle('Add Title Page Information'); // Set the title of the sidebar.
   DocumentApp.getUi() 
       .showSidebar(html); // Display the HTML file as a sidebar.
 }
@@ -18,20 +18,20 @@ function opentitlePageDialog()
 function openTestDialog()
 {
   var html = HtmlService.createHtmlOutputFromFile('Tests'); 
-  html.setTitle("Add Tests"); 
+  html.setTitle('Add Tests'); 
   DocumentApp.getUi() 
       .showSidebar(html); 
 }
 
 function populateGeneralInformation(form)
 {
-  var marker = ['{Test Plan Name}', '{Name}', '{Date}', '{Jira #}', '{Part name}', '{Part number}', '{Project name}', '{testSummary}'];
-  var fieldName = [form.testPlanName, form.fullName, form.date, form.jiraTicketNumber, form.partName, form.partNumber, form.projectName, form.testSummary]; // I learned that arrays can store a class with a property, like form.fullName. Arrays can store any data type!
-  var body = DocumentApp.getActiveDocument().getBody();
-  var footer = DocumentApp.getActiveDocument().getFooter().getParent().getChild(4); // Because the first page has a different footer, DocumentApp.getActiveDocument().getFooter(); would check only the first page.
+  const marker = ['{Test Plan Name}', '{Name}', '{Date}', '{Jira #}', '{Part name}', '{Part number}', '{Project name}', '{testSummary}'];
+  const fieldName = [form.testPlanName, form.fullName, form.date, form.jiraTicketNumber, form.partName, form.partNumber, form.projectName, form.testSummary];
+  const body = DocumentApp.getActiveDocument().getBody();
+  const footer = DocumentApp.getActiveDocument().getFooter().getParent().getChild(4); // Because the first page has a different footer, DocumentApp.getActiveDocument().getFooter(); would check only the first page.
   for (i=0; i<marker.length; i++)
   {
-    if (fieldName[i] != "")
+    if (fieldName[i] != '')
     {
       body.replaceText(marker[i], fieldName[i]); // First argument is the location in the document, second argument is the matching text field on the sidebar.
       footer.replaceText(marker[i], fieldName[i]); 
@@ -41,121 +41,108 @@ function populateGeneralInformation(form)
   storeData(form.date);
 }
 
-function populateTestSpecificInformation(form)
+function populateTestSpecificInformation(test)
 {
-  var marker = [];
-  var fieldName = [];
-  var body = DocumentApp.getActiveDocument().getBody();
+  let marker = [];
+  const body = DocumentApp.getActiveDocument().getBody();
+  let limit = 0;
 
-  switch (form.hiddenTestName)
+  switch (test.name)
   { 
-    case "1": /* "coldSoakVariables" */
+    case 'coldSoakVariables':
       marker = ['{tooling}', '{notes}', '{dwellTemp}', '{dwellTime}', '{minVoltage}', '{maxVoltage}'];
-      fieldName = [form.tooling, form.notes, form.dwellTemp, form.dwellTime, form.minVoltage, form.maxVoltage];
       break;
-    case "2": /*"hotSoakVariables" */
+    case 'hotSoakVariables':
       marker = ['{tooling}', '{notes}', '{dwellTemp}', '{dwellTime}', '{minVoltage}', '{maxVoltage}', '{steadyTime}', '{voltageRange}'];
-      fieldName = [form.tooling, form.notes, form.dwellTemp, form.dwellTime, form.minVoltage, form.maxVoltage, form.steadyTime, form.voltageRange];
       break;
-    case "3": /*"hotStorageVariables"*/
+    case 'hotStorageVariables':
       marker = ['{tooling}', '{notes}', '{dwellTemp}', '{dwellTime}'];
-      fieldName = [form.tooling, form.notes, form.dwellTemp, form.dwellTime];
       break;
-    case "4": /*"coldStorageVariables"*/
+    case 'coldStorageVariables':
       marker = ['{tooling}', '{notes}', '{dwellTemp}', '{dwellTime}'];
-      fieldName = [form.tooling, form.notes, form.dwellTemp, form.dwellTime];
       break;
-    case "5": /*"thermalCycleVariables"*/
-      marker = ['{tooling}', '{notes}', '{minTemp}', '{maxTemp}', '{dwellTime}', '{cycles}'];
-      fieldName = [form.tooling, form.notes, form.minTemp, form.maxTemp, form.dwellTime, form.cycles]; 
+    case 'thermalCycleVariables':
+      marker = ['{tooling}', '{notes}', '{minTemp}', '{maxTemp}', '{dwellTime}', '{cycles}']; 
       break;
-    case "6": /*"thermalShockVariables"*/
+    case 'thermalShockVariables':
       marker = ['{tooling}', '{notes}', '{minTemp}', '{maxTemp}', '{dwellTime}', '{shockEvents}'];
-      fieldName = [form.tooling, form.notes, form.minTemp, form.maxTemp, form.dwellTime, form.shockEvents];  
       break;
-    case "7": /*"enduranceVibrationVariables"*/ 
+    case 'enduranceVibrationVariables':
       marker = ['{notes}'];
-      fieldName = [form.notes];
       break;
-    case "8": /* "chemicalExposureVariables" */
+    case 'chemicalExposureVariables':
       marker = ['{notes}'];
-      fieldName = [form.notes];
       break;
-    case "9": /* "functionalVibrationVariables" */
+    case 'functionalVibrationVariables':
       marker = ['{notes}'];
-      fieldName = [form.notes];
       break;
-    case "10": /* "istaDropVariables" */
+    case 'istaDropVariables':
       marker = ['{tooling}', '{notes}'];
-      fieldName = [form.tooling, form.notes];
       break;
-    case "11": /* "transitDropVariables" */
+    case 'transitDropVariables':
       marker = ['{tooling}', '{notes}'];
-      fieldName = [form.tooling, form.notes];
       break;
-    case "12": /* humidityToleranceVariables*/
-    marker = ['{notes}', '{cycles}', '{cycleTime}', '{dwellTime}'];
-    fieldName = [form.notes, form.cycles, form.cycleTime, form.dwellTime];
+    case 'humidityToleranceVariables':
+      marker = ['{notes}', '{cycles}', '{cycleTime}', '{dwellTime}'];
     break;
-    case "13": /* corrosionVariables*/
-    marker = ['{notes}', '{days}', '{cycles}'];
-    fieldName = [form.notes, form.days, form.cycles];
+    case 'corrosionVariables':
+      marker = ['{notes}', '{days}', '{cycles}'];
     break;
   }
-    for (i=0; i<marker.length; ++i)
+    limit = marker.length;
+    for (let i = 0; i < limit; i++)
     {
-      if (fieldName[i] != "")
+      if (test.variables[i] != '')
       {
-        body.replaceText(marker[i], fieldName[i]); // First argument is the location in the document. Second argument is the matching text field on the sidebar.
+        body.replaceText(marker[i], test.variables[i]); // First argument is the location in the document. Second argument is the matching text field on the sidebar.
       }
     }
 }
 
-function addNewTest(form)
+function addNewTest(testName)
 {
-  var selectedTest = form.tests;
-  switch (selectedTest)
+  switch (testName)
   {
-    case "thermalCycle":
+    case 'thermalCycle':
       appendTest('https://docs.google.com/document/d/1pdqgD7eWGZfip1K795DE0Pal0qG5NsNTxAe-CfJyXkQ/edit');
       break;
-    case "chemicalExposure":
+    case 'chemicalExposure':
       appendTest('https://docs.google.com/document/d/1BMM0rXBD-rB-9tWraeDT6lnCEUSx3timBVt86inHNbs/edit');
       break;
-    case "coldSoak":
+    case 'coldSoak':
       appendTest('https://docs.google.com/document/d/1dsrzhk2AqhhJIS47mieGwTNUmeFL2l9tB4xWlJkEJ5c/edit');
       break;
-    case "hotSoak":
+    case 'hotSoak':
       appendTest('https://docs.google.com/document/d/1_MVYJUq45_QSDnc6KTuh1Fkula_Lp8--K_u30FLsC5s/edit');
       break;
-    case "coldStorage":
+    case 'coldStorage':
       appendTest('https://docs.google.com/document/d/1OklUjVlpdcEsk4V0dS-NHteJxE2J1wpF7LWyfisrHrY/edit');
       break;
-    case "hotStorage":
+    case 'hotStorage':
       appendTest('https://docs.google.com/document/d/1oC4i4jLXp2xG-5yAV8D80vmewMoralx14RWngX4NM3w/edit');
       break;
-    case "enduranceVibration":
+    case 'enduranceVibration':
       appendTest('https://docs.google.com/document/d/1X4U0m360d6m9b4cdz9GGlRUsFOo53nmyHgL6bhqECSA/edit');
       break;
-    case "functionalVibration":
+    case 'functionalVibration':
       appendTest('https://docs.google.com/document/d/1gPzlT8K64NxwVMyoyGHMndo2NtuQcqqnLGrISwMIIbI/edit#');
       break;
-    case "istaDrop":
+    case 'istaDrop':
       appendTest('https://docs.google.com/document/d/1qL0A0cFN5nbHcxq0lR9vMlKd1ckAvrletOPKzMPJ0HI/edit');
       break;
-    case "thermalShock":
+    case 'thermalShock':
       appendTest('https://docs.google.com/document/d/1mHgwVLi0PeGopI6vLiTdW1vamGXcJ6Av-RTre7wxxec/edit');
       break;
-    case "transitDrop":
+    case 'transitDrop':
       appendTest('https://docs.google.com/document/d/1LvmJm6EVaJkp0D2ZrqGiyUD8oDWLpco9nU1ohgh6ems/edit');
       break;
-    case "userDefined":
+    case 'userDefined':
       appendTest('https://docs.google.com/document/d/1xRd1Cn_6zIMeH-tmcAxBZgPSzO605cfQaviyoGXuUUk/edit');
       break;
-    case "humidityTolerance":
+    case 'humidityTolerance':
       appendTest('https://docs.google.com/document/d/1qYYNoMIdfjbaSpwEVouJIODRPGIe44xwdiOX4D7fQF0/edit#')
       break;
-    case "corrosion":
+    case 'corrosion':
       appendTest('https://docs.google.com/document/d/15o-8TEO7IkKZgzghOEFed5YeUzcE2VdsEcPXQ09wewo/edit#')
       break;          
   }
@@ -163,70 +150,69 @@ function addNewTest(form)
 
 function appendTest(testID)
 {
-  var thisDoc = DocumentApp.getActiveDocument();
-  var thisBody = thisDoc.getBody();
-  var templateDoc = DocumentApp.openByUrl(testID); // Pass in id of doc to be used as a template.
-  var templateBody = templateDoc.getBody();
-  var sizeOfItems = {};
+  const thisDoc = DocumentApp.getActiveDocument();
+  const thisBody = thisDoc.getBody();
+  let templateDoc = DocumentApp.openByUrl(testID); // Pass in id of doc to be used as a template.
+  const templateBody = templateDoc.getBody();
+  const elType = DocumentApp.ElementType;
+  const itemType = DocumentApp.GlyphType;
+  let sizeOfItems = {};
   sizeOfItems[DocumentApp.Attribute.FONT_SIZE] = 10;
 
-  for (var i = 0; i < templateBody.getNumChildren(); i++)
-  { // Run through the elements of the template doc's Body.
-    switch (templateBody.getChild(i).getType())
-    { // Deal with the various types of Elements we will encounter and append.
-      case DocumentApp.ElementType.PARAGRAPH:
+  for (let i = 0; i < templateBody.getNumChildren(); i++) { // Run through the elements of the template doc's Body.
+    switch (templateBody.getChild(i).getType()) { // Deal with the various types of Elements we will encounter and append.
+      case elType.PARAGRAPH:
         thisBody.appendParagraph(templateBody.getChild(i).copy());
         break;
-      case DocumentApp.ElementType.LIST_ITEM:
-        var typeOfList = templateBody.getChild(i).getGlyphType(); // Determine the type of the list item. Returns an object.
-        var typeOfListAsString = JSON.stringify(typeOfList); // Convert the type from an object to a string that will be used in the if statement.
-        if (typeOfListAsString = "NUMBER")
+      case elType.LIST_ITEM:
+        let typeOfList = templateBody.getChild(i).getGlyphType(); // Determine the type of the list item. Returns an object.
+        let typeOfListAsString = typeOfList.toString(); // Convert the type from an object to a string that will be used in the if statement.
+        if (typeOfListAsString === 'NUMBER')
         {
-          var item = thisBody.appendListItem(templateBody.getChild(i).copy());
+          let item = thisBody.appendListItem(templateBody.getChild(i).copy());
           item.setAttributes(sizeOfItems);
-          item.setGlyphType(DocumentApp.GlyphType.NUMBER);
-        } else if (typeOfListAsString = "BULLET")
+          item.setGlyphType(itemType.NUMBER);
+        } else if (typeOfListAsString === 'BULLET')
         {
-          thisBody.appendListItem(templateBody.getChild(i).copy()).setGlyphType(DocumentApp.GlyphType.BULLET);
+          thisBody.appendListItem(templateBody.getChild(i).copy()).setGlyphType(itemType.BULLET);
         } else {
-          thisBody.appendListItem(templateBody.getChild(i).copy()).setGlyphType(DocumentApp.GlyphType.LATIN_LOWER);
+          thisBody.appendListItem(templateBody.getChild(i).copy()).setGlyphType(itemType.LATIN_LOWER);
         }
         break;
-      case DocumentApp.ElementType.TABLE:
+      case elType.TABLE:
         thisBody.appendTable(templateBody.getChild(i).copy());
         break;
-      case DocumentApp.ElementType.INLINE_IMAGE:
+      case elType.INLINE_IMAGE:
         thisBody.appendImage(templateBody.getChild(i).copy());
         break;
     }
   }
-  var doc = DocumentApp.getActiveDocument();
-  var paragraphs = doc.getBody().getParagraphs();
-  var position = doc.newPosition(paragraphs[paragraphs.length - 1], 0);
-  doc.setCursor(position);
-  thisBody.appendPageBreak(); // Add a page break once the entire test has been copied.
+  let paragraphs = thisDoc.getBody().getParagraphs();
+  let position = thisDoc.newPosition(paragraphs[paragraphs.length - 1], 0);
+  thisDoc.setCursor(position);
+  thisBody.appendPageBreak();
 }
 
 function nameFile(type, number, name, date)
 {
-  DocumentApp.getActiveDocument().setName(type + " - " + number + " - " + name + " - " + date);
+  DocumentApp.getActiveDocument().setName(type + ' - ' + number + ' - ' + name + ' - ' + date);
 }
 
 function storeData() 
 {
-  var database = FirebaseApp.getDatabaseByUrl("https://ctct-environmental-test-plans.firebaseio.com/", "xTw3pwH5Gt8lZk4t9FgA2hpTtblfz0J7azfnM2sD");
-  var email = Session.getActiveUser().getEmail(); // Get the user's email.
-  var date = new Date();
-  var docName = DocumentApp.getActiveDocument().getName();
-  var storageLocation = '';
+  const database = FirebaseApp.getDatabaseByUrl('https://ctct-environmental-test-plans.firebaseio.com/', 'xTw3pwH5Gt8lZk4t9FgA2hpTtblfz0J7azfnM2sD');
+  let email = Session.getActiveUser().getEmail(); // Get the user's email.
+  const date = new Date();
+  let docName = DocumentApp.getActiveDocument().getName();
+  let storageLocation = '';
 
-  date = (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear();
-  var userInfo = {
+  let day = (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear();
+  const userInfo = {
     email: email,
-    date: date,
+    date: day,
     docName: docName
   }
-  email = email.split("@", 1);
-  storageLocation = email + Math.round(Math.random() * 10000).toString();
+  email = email.split('@', 1);
+  storageLocation = email + day + (date.getTime()).toString();
   database.setData(storageLocation, userInfo);
 }
